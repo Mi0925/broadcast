27e565cfb81cbe0323a71c2e469108cb9a76519a
xuyiyang <1141767477@qq.com>
2017-11-30 16:22:44 +0800

    添加左侧导航

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,21 @@
 // JavaScript Document
 $(document).ready(function(){
-	$("header").load("header.html");
-	$("footer").load("footer.html");
+	// 左边一级导航
+	$(".gb-left-nav").on("click",".nav-tit",function(){
+		var $this=$(this).parents("li");
+		$this.find(".left-sub-nav").slideToggle();
+		$this.siblings('li').find('.left-sub-nav').slideUp();
+		$this.addClass("cur").siblings("li").removeClass("cur");
+		$this.siblings('li').find(".arrow").removeClass("icon-arrow-bottom").addClass("icon-arrow-right")
+		if($this.find(".arrow").hasClass('icon-arrow-right')){
+			$this.find(".arrow").removeClass("icon-arrow-right").addClass("icon-arrow-bottom")
+		}else{
+			$this.find(".arrow").removeClass("icon-arrow-bottom").addClass("icon-arrow-right")
+		}
+	})
+	// 左边二级导航
+	$(".gb-left-nav").on("click",".left-sub-nav a",function(){
+		$(this).addClass("cur").siblings("a").removeClass('cur');
+		$(this).parents("li").siblings("li").find("a").removeClass('cur');
+	})
 })
