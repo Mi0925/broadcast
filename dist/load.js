40012ceaf53de9f37f6eb8b91bef4d0c52afa196
xuyiyang <1141767477@qq.com>
2017-12-04 15:40:41 +0800

    首页调整

--- dist/load.js
+++ dist/load.js
@@ -1,2 +1,16 @@
 $(".gb-left-nav").load("nav-left.html");//左侧导航
 $(".gb-main-content").load("round.html");//首页
+
+
+// 页面切换
+function gbMainContent(url) {
+	for(var i = 1; i < 1000; i++) {
+		clearInterval(i);
+	}
+	$(".gb-main-content").load(url);//首页
+}
+
+
+
+
+
