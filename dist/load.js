9ae48ea4c97b541e728489b5bf14ebee80461c3c
xuyiyang <1141767477@qq.com>
2017-12-05 12:01:56 +0800

    首页初版

--- dist/load.js
+++ dist/load.js
@@ -4,9 +4,9 @@ $(".gb-main-content").load("round.html");//首页
 
 // 页面切换
 function gbMainContent(url) {
-	for(var i = 1; i < 1000; i++) {
-		clearInterval(i);
-	}
+	// for(var i = 1; i < 100; i++) {
+	// 	clearInterval(i);
+	// }
 	$(".gb-main-content").load(url);//首页
 }
 
