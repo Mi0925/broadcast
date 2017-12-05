b497e9851799caff1d0c433ba74a05701fca3ad6
xuyiyang <1141767477@qq.com>
2017-12-05 16:50:17 +0800

    文件布局改版

--- dist/load.js
+++ dist/load.js
@@ -1,13 +1,11 @@
-$(".gb-left-nav").load("nav-left.html");//左侧导航
-$(".gb-main-content").load("round.html");//首页
+$(".gb-main-content").load("components/index.html");//首页
 
 
 // 页面切换
 function gbMainContent(url) {
-	// for(var i = 1; i < 100; i++) {
-	// 	clearInterval(i);
-	// }
-	$(".gb-main-content").load(url);//首页
+	$(".gb-main-content").load(url,function(){
+		clearInterval(RoundaboutSet);//清除首页轮播定时器
+	});
 }
 
 
