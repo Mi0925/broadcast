07d7d1eb78a0be719718baad338e10a64bc2653b
xuyiyang <1141767477@qq.com>
2018-01-03 10:33:12 +0800

    第一批bug修改

--- src/js/tree.js
+++ src/js/tree.js
@@ -1,35 +1,36 @@
 // zTree 的参数配置，深入使用请参考 API 文档（setting 配置详解）
-var selSetting = {
-	view:{
-		showLine:false,//去掉连接线
-		addDiyDom:addDiyDom
-	}
-};
-var allSetting = {
-	view:{
-		showLine:false,//去掉连接线
-	},
-	check:{//添加复选框
-		enable:true,
-	}
-};
+// var selSetting = {
+// 	view:{
+// 		showLine:false,//去掉连接线
+// 		addDiyDom:addDiyDom
+// 	}
+// };
+// var allSetting = {
+// 	view:{
+// 		showLine:false,//去掉连接线
+// 	},
+// 	check:{//添加复选框
+// 		enable:true,
+// 	}
+// };
+
+
+// 初始化树
+// function ztree_Initialize(argument) {
+// 	//带关闭按钮的树
+// 	$(".ztree-close").each(function() {
+// 		$.fn.zTree.init($(this), selSetting, zNodes);
+// 	});
+// 	//带复选框的树
+// 	$(".ztree-check").each(function() {
+// 		$.fn.zTree.init($(this), allSetting, zNodes);
+// 	});
+// }
 
 // 添加树关闭按钮点击事件
 $("body").on('click', '.ztree .tree-close', function(event) {
 	removeDom($(this).parents(".ztree").attr("id"))
 });
-// 初始化树
-function ztree_Initialize(argument) {
-	//带关闭按钮的树
-	$(".ztree-close").each(function() {
-		$.fn.zTree.init($(this), selSetting, zNodes);
-	});
-	//带复选框的树
-	$(".ztree-check").each(function() {
-		$.fn.zTree.init($(this), allSetting, zNodes);
-	});
-}
-
 // 添加树关闭按钮
 function addDiyDom(treeId, treeNode) {
 	var aObj = $("#" + treeNode.tId + "_a");
